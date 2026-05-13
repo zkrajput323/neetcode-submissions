cf5b47f657078042a239031ef36ce1ecc1ee31b9
zkrajput323 <kkumar47347@ucumberlands.edu>
2026-05-12 20:01:13 -0500

    Bulk sync: 13 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      //   HashSet<INTEGER> set = new HashSet<>();
+
+      HashMap<Integer,Integer> map = new HashMap<>();
+
+      // create the fequecy map
+
+      for(int i = 0; i < nums.length; i++){
+         int complementry = target - nums[i];
+
+        // store the value as key and index as value
+        if(map.containsKey(complementry)){
+
+          return new int[]{map.get(complementry), i};
+
+        }else{
+          
+          map.put(nums[i], i);
+        }
+        
+      }
+      
+        return new int[]{};
+    }
+}
