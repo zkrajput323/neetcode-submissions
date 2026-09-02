70346e4c867e829900807e7e7162a62f68e00b68
zkrajput323 <kkumar47347@ucumberlands.edu>
2026-09-01 23:34:24 -0500

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #created the empty map
+        freq = dict()
+        indicies = list()
+
+        for i,num in enumerate(nums):
+            diff = target - nums[i]
+
+            if(diff in freq):
+            #so here rather than using .get method ( extra safty behaviour.)  I can use this freq[diff] ( fetch the value)
+                # indicies.append(freq.get(diff))
+                indicies.append(freq[diff])
+                indicies.append(i)
+                return indicies
+            freq[num] = i                
+
+
